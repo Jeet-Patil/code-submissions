3429e9e6eb1e106f46a9e8452adaea96c0af5b16
Jeet Patil <jeetpatil3333@gmail.com>
2026-06-22 11:23:44 +0530

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0;
+        int res = 0;
+        unordered_map <char, int> mp;
+        for (int r = 0; r < s.size(); r++) {
+            if (mp.find(s[r]) != mp.end()) {
+                l = max(mp[s[r]] + 1, l);
+            }
+            mp[s[r]] = r;
+            res = max(res, r - l + 1);
+            cout << res << " " << l << " " << r << endl;
+        }
+        return res;
+    }
+};
